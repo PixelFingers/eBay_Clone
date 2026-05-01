8892ac0c9a3369c9a252e3c9c59332960a6bd835
unknown <charon19.exe@gmail.com>
2026-05-01 12:15:16 +0530

    added images for mail

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,31 +1,29 @@
-const sgMail = require("@sendgrid/mail")
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
 const sendMail = async (to, data, pdfBuffer) => {
   try {
+    const attachments = []
+
+    if (pdfBuffer && pdfBuffer.length > 0) {
+      attachments.push({
+        content: Buffer.from(pdfBuffer).toString("base64"),
+        filename: "invoice.pdf",
+        type: "application/pdf",
+        disposition: "attachment"
+      })
+    }
+
     await sgMail.send({
       to,
       from: process.env.EMAIL_USER,
-
       templateId: "d-e9acec6f9dd349feb60a299c5a6c33e2",
-
       dynamic_template_data: data,
-      attachments: pdfBuffer ? [
-            {
-              content: pdfBuffer.toString("base64"),
-              filename: "invoice.pdf",
-              type: "application/pdf",
-              disposition: "attachment"
-            }
-          ]
-        : []})
+      attachments
+    })
+
     console.log("MAIL SENT")
     return true
+
   } catch (err) {
     console.log("MAIL ERROR:", err.response?.body || err.message)
     return false
   }
 }
-
-module.exports = sendMail
